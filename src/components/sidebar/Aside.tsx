9aaf21189bd1427e9c74fe1e9a0c45b2360a85c2
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-20 17:03:28 +0530

    aside bar border color changed

--- src/components/sidebar/Aside.tsx
+++ src/components/sidebar/Aside.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Aside = () => {
   const [buttons, setButtons] = useState(asideButtonsNames);
   return (
-    <aside className="bg-white row-span-full flex flex-col p-2 border-[1px] border-black">
+    <aside className="bg-white row-span-full flex flex-col p-2 border-[1px] border-gray-300 rounded-md">
       {buttons.map((name) => {
         return <AsideButton key={name} name={name} />;
       })}
--- src/components/sidebar/AsideButton.tsx
+++ src/components/sidebar/AsideButton.tsx
@@ -5,11 +5,11 @@ import GetAsideButtonIcon from "./GetAsideButtonIcon";
 const AsideButton: FC<AsideButtonProps> = ({ name }) => {
   return (
     <article
-      className={`flex-auto px-4 text-gray-600 cursor-pointer flex flex-col gap-y-2 justify-center items-center rounded-md ${
+      className={`flex-auto px-4 cursor-pointer flex flex-col gap-y-2 justify-center items-center rounded-md ${
         name === "Home" ? "bg-blue-200" : ""
       }`}
     >
-      <button className="text-3xl">
+      <button className="text-3xl text-gray-600">
         <GetAsideButtonIcon name={name} />
       </button>
       <span className="text-xs">{name}</span>
